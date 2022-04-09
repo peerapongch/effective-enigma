659a18b53aa54e739950287c91a647c20ec03488
Peerapong <p.chuanromanee@gmail.com>
2022-04-09 23:46:32 +0700

    to colab

--- LEDScraper/Postprocess.py
+++ LEDScraper/Postprocess.py
@@ -1,7 +1,19 @@
+from selenium import webdriver
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.chrome.options import Options
+
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
 import pandas as pd
 import numpy as np
+import time
 from datetime import datetime
 from dateutil.relativedelta import relativedelta
+
+from .Mappers import *
 
 def run_postprocess(out_dict):
 
@@ -62,6 +74,9 @@ def run_postprocess(out_dict):
     data[auction_status_cols] = data[auction_status_cols].fillna('NA')
     data[auction_date_cols] = data[auction_date_cols].fillna('01/01/2022') # some arbitrary date in the past
 
+    # cleaning: deed number
+    data['deed_no'] = data['deed_no'].apply(clean_deed)
+
     # cleaning: auction identifiers
     data['auction_identifier'] = data[['lot_no','sequence_no']].agg('|'.join, axis=1)
 
@@ -105,3 +120,162 @@ def run_postprocess(out_dict):
     new_cols = focus_cols + remaining_cols
     
     return data[new_cols]
+
+def get_deed_range(x):
+    
+    from_deed = int(x.split('-')[0].strip())
+    to_deed = int(x.split('-')[1].strip())+1
+    
+    if to_deed < from_deed:
+        to_deed = int(
+            str(from_deed)[:-1] + str(to_deed)
+        )
+        
+    return list(
+        range(
+            from_deed,
+            to_deed
+        )
+    )
+    
+def clean_deed(deed_str):
+    
+    try:
+        deed_list = [x for x in deed_str.split(',') if len(x)>0]
+        deed_list = [
+            [int(x)] if x.find('-')==-1 else get_deed_range(x) for x in deed_list
+        ]
+        deed_list_int = [elem for entry in deed_list for elem in entry]
+        return str(deed_list_int).replace('[','').replace(']','').replace(' ','')
+    except:
+        return deed_str
+
+def find_location(
+    entry,
+    driver=None,
+    max_locations_per_entry=3,
+    max_wait_time=5,
+    driver_path='./chromedriver.exe',
+    led_search_url='https://landsmaps.dol.go.th/',
+    headless=True
+    ):
+
+    if not driver:
+        options = Options()
+        options.headless = headless
+        
+        driver = webdriver.Chrome("./chromedriver.exe", options=options)
+        driver.get(led_search_url)
+
+        wait = WebDriverWait(driver, 60)\
+        .until(
+            EC.presence_of_element_located(
+                (By.XPATH, '/html/body/nav/form[1]/div/select')
+            )
+        )
+        prov_select = Select(driver.find_elements_by_xpath('/html/body/nav/form[1]/div/select')[0])
+        prov_select.select_by_visible_text('กรุงเทพมหานคร')
+
+
+    possible_locations = []
+    
+    if entry['deed_no'] is not np.nan:
+
+        entry_province = entry['province']
+        entry_district = entry['district']
+        possible_districts = district_mapper[entry_province][entry_district.replace(' ','')]
+
+        # print(possible_districts)
+
+        for d in possible_districts:
+            
+            # print(entry['deed_no'].split(','))
+            
+            for deed in entry['deed_no'].split(','):
+
+                district_select = Select(driver.find_elements_by_xpath('/html/body/nav/form[2]/div/select')[0])
+                district_select.select_by_visible_text(d)
+
+                deed_box = driver\
+                .find_elements_by_xpath(
+                    '/html/body/nav/form[3]/span/input'
+                )[0]
+
+                deed_box.clear()
+                deed_box.send_keys(deed)
+                deed_box.send_keys(Keys.ENTER)
+
+                try:
+                    # elemets = [
+                    #     '/html/body/div[1]/div[3]/span/div/div[2]/div[2]/div/div[2]/div[10]/div[2]/a',
+                    #     '/html/body/div[1]/div[3]/span/div/div[2]/div[2]/div/div[2]/div[10]/div[2]/a',
+                    #     '/html/body/div[1]/div[3]/span/div/div[2]/div[2]/div/div[2]/div[9]/div[2]',
+                    #     '/html/body/div[1]/div[3]/span/div/div[2]/div[2]/div/div[2]/div[9]/div[2]'
+                    # ]
+                    # for elem in elements:
+                    #     WebDriverWait(driver, max_wait_time/4)\
+                    #     .until(
+                    #         EC.presence_of_element_located(
+                    #             (By.XPATH, elem)
+                    #         )
+                    #     )
+                    # time.sleep(max_wait_time)
+                    # WebDriverWait(driver, max_wait_time)
+                    wait = WebDriverWait(driver, max_wait_time)\
+                    .until(
+                        EC.presence_of_element_located(
+                            (By.XPATH, '/html/body/div[1]/div[3]/span/div/div[2]/div[2]/div/div[2]/div[9]/div[2]')
+                        )
+                    )
+                    
+                    coor = driver.find_elements_by_xpath(
+                        '/html/body/div[1]/div[3]/span/div/div[2]/div[2]/div/div[2]/div[10]/div[2]/a'
+                    )[0].text
+
+                    gmap = driver.find_elements_by_xpath(
+                        '/html/body/div[1]/div[3]/span/div/div[2]/div[2]/div/div[2]/div[10]/div[2]/a'
+                    )[0].get_attribute('href')
+
+                    price = driver.find_elements_by_xpath(
+                        '/html/body/div[1]/div[3]/span/div/div[2]/div[2]/div/div[2]/div[9]/div[2]'
+                    )[0].text.split(' ')[0]
+
+                    price_unit = driver.find_elements_by_xpath(
+                        '/html/body/div[1]/div[3]/span/div/div[2]/div[2]/div/div[2]/div[9]/div[2]'
+                    )[0].text.split(' ')[1]
+
+                    print(f'District: {d}; Deed: {deed}; Coordinates: {coor}; Price {price} {price_unit}; Gmap: {gmap}')
+                    
+                    possible_locations.append(
+                        [
+                            coor,
+                            gmap,
+                            f'{price} {price_unit}',
+                            d,
+                            deed
+                        ]
+                    )
+                    
+                    if len(possible_locations) == max_locations_per_entry:
+                        break
+                        
+                except Exception as e:
+                    
+                    print(f'District: {d}; Deed: {deed}; Coordinates: NA; Price NA; Gmap: NA')
+                    # raise(e)
+                    pass
+                    
+    if len(possible_locations)==0:
+        possible_locations.append(['NA']*5)
+        
+    # print(possible_locations)
+    results = [*possible_locations[0],str(possible_locations)]
+    # print(results)
+    
+    return pd.Series(results)
+
+def par_find_location(
+    data,
+    n_partitions=4
+    ):
+    pass
