110475d9b1bab3aee0b697fb49702f85a8046f5c
Peerapong <p.chuanromanee@gmail.com>
2022-04-16 15:14:43 +0700

    minor

--- LEDScraper/Postprocess.py
+++ LEDScraper/Postprocess.py
@@ -319,7 +319,7 @@ def find_location(
                 finally:
                     print(f'Terminate: {str(terminate)}')
                     print(f'Possible loc: {len(possible_locations)}')
-                    print(possible_locations[0])
+                    print(possible_locations)
 
             if terminate:
                 break
