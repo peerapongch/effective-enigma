0aea02af790bc0330f812e2341b6cae1a4e3d140
Peerapong <p.chuanromanee@gmail.com>
2022-04-16 14:41:55 +0700

    added no-sandbox

--- run_scraper.py
+++ run_scraper.py
@@ -41,6 +41,7 @@ if __name__ == "__main__":
 	if HEADLESS:
 		options.add_argument("--disable-web-security")
 		options.add_argument("--disable-site-isolation-trials")
+		options.add_argument('--no-sandbox')
 		options.add_argument("--headless")
 		options.add_argument("--disable-gpu")
 		user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.88 Safari/537.36'
